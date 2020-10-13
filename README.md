2b017f98c8fdbe178265fb454b712412a227b063
Chin Valery <chinvalery@gmail.com>
2020-10-13 18:38:35 +0200

    responsive nav bar

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# wildo_vuejs
+# navbar
+
+It's a fully responsive navbar component with full feature to customize css and styles
+
+I don’t personally created/have credits on this post. I walk through DevEd responsive navbar tutorial and add a dropdown feature to it!
+Thank you DevEd for your great works we all appreciate you ❤
+
+[Mediuim Post](https://medium.com/@hasanparasteh/ready-to-use-vue-js-navbar-bf79c43af6da)
+
+[DevEd Tutorial](https://youtu.be/gXkqy0b4M5g)
+
+## Project setup
+
+```bash
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```bash
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```bash
+npm run build
+```
+
+### Lints and fixes files
+
+```bash
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Navbar
+      name="Wildo Codak" 
+      :navLinks="[
+        {
+          name: 'Home',
+          link: '/home',
+          dropdown: false,
+        },
+
+        { name: 'About', 
+        link: '/about', 
+        dropdown: false },
+
+        {
+          name: 'Contacts',
+          link: '/contact',
+          dropdown: true,
+          dropdownLinks: [ 
+            { name: 'A', link: '/pA', dropdown: false },
+            { name: 'B', link: '/pB', dropdown: false },
+          ],
+        },
+        {
+          name: 'Projects',
+          link: '/projects',
+          dropdown: false,
+        },
+      ]"
+    />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  overflow-x: hidden;
+}
+</style>
