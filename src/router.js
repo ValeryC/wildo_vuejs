9f3073b4b80d0e8d95341488f6c0d5a0c6b2b57b
Chin Valery <chinvalery@gmail.com>
2020-10-16 06:25:35 +0200

    add new component invisible2

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Invisible from './views/Invisible';
+import Invisible2 from './views/Invisible2';
 import Home from './views/Home.vue';
 import Gallery from './views/Gallery';
 import Projects from './views/Projects';
@@ -41,6 +42,10 @@ export default new Router({
     {
       path: '/invisible',
       component: Invisible
+    },
+    {
+      path: '/invisible2',
+      component: Invisible2
     }
   ]
 });
